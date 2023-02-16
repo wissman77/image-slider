6de4bbd4b7eda1e5e35f8899cb17f138b58346d1
Wissam Mansour <wissman77@gmail.com>
2023-02-16 22:21:19 +0200

    add IIFE function

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
-let index = 0;
+const imageSlider = (() => {
+  let index = 0;
 
-const images = document.querySelectorAll('.slide');
-const dots = document.querySelectorAll('.dot');
+  const images = document.querySelectorAll('.slide');
+  const dots = document.querySelectorAll('.dot');
 
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
+  const prev = document.querySelector('.prev');
+  const next = document.querySelector('.next');
 
-prev.addEventListener('click', () => slide('prev'));
-next.addEventListener('click', () => slide('next'));
+  prev.addEventListener('click', () => slide('prev'));
+  next.addEventListener('click', () => slide('next'));
 
-const slides = document.querySelector('.slides');
+  const slides = document.querySelector('.slides');
 
-slides.addEventListener('mouseover', () => {
-  next.classList.add('visible');
-  prev.classList.add('visible');
-});
+  slides.addEventListener('mouseover', () => {
+    next.classList.add('visible');
+    prev.classList.add('visible');
+  });
 
-slides.addEventListener('mouseout', () => {
-  next.classList.remove('visible');
-  prev.classList.remove('visible');
-});
+  slides.addEventListener('mouseout', () => {
+    next.classList.remove('visible');
+    prev.classList.remove('visible');
+  });
 
-function slide(direction) {
-  if (direction === 'next') {
-    index++;
-  } else {
-    index--;
+  function slide(direction) {
+    if (direction === 'next') {
+      index++;
+    } else {
+      index--;
+    }
+    images.forEach((imageDiv) => {
+      imageDiv.classList.remove('active');
+    });
+
+    dots.forEach((dot) => {
+      dot.classList.remove('active-dot');
+    });
+
+    images[index % images.length].classList.add('active');
+    dots[index % images.length].classList.add('active-dot');
   }
-  images.forEach((imageDiv) => {
-    imageDiv.classList.remove('active');
-  });
 
-  dots.forEach((dot) => {
-    dot.classList.remove('active-dot');
-  });
-
-  images[index % images.length].classList.add('active');
-  dots[index % images.length].classList.add('active-dot');
-}
-
-setInterval(() => {
-  slide('next');
-}, 5000);
+  setInterval(() => {
+    slide('next');
+  }, 5000);
+})();
