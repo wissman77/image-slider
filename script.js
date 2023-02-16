bcc7bd25ae8242584d3130b14a12c0f5437add62
Wissam Mansour <wissman77@gmail.com>
2023-02-16 22:25:27 +0200

    add comments

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const imageSlider = (() => {
+  // variable to change the images and dots
   let index = 0;
 
+  // all slides and dots elements
   const images = document.querySelectorAll('.slide');
   const dots = document.querySelectorAll('.dot');
 
+  // selector of next and previous btns
   const prev = document.querySelector('.prev');
   const next = document.querySelector('.next');
 
+  // event listeners for the click on btns
   prev.addEventListener('click', () => slide('prev'));
   next.addEventListener('click', () => slide('next'));
 
@@ -28,6 +32,8 @@ const imageSlider = (() => {
     } else {
       index--;
     }
+
+    // remove active images and dots
     images.forEach((imageDiv) => {
       imageDiv.classList.remove('active');
     });
@@ -36,6 +42,7 @@ const imageSlider = (() => {
       dot.classList.remove('active-dot');
     });
 
+    // add active class to images and dots
     images[index % images.length].classList.add('active');
     dots[index % images.length].classList.add('active-dot');
   }
